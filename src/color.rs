0ac3eee1920dee38211b4be12cd41b3202cb4980
gram <git@orsinium.dev>
2024-03-22 21:25:03 +0100

    implement color adapter

--- src/color.rs
+++ src/color.rs
@@ -1,15 +1,13 @@
-use core::marker::PhantomData;
-
 use crate::state::State;
 use embedded_graphics::geometry::OriginDimensions;
-use embedded_graphics::pixelcolor::{Gray2, IntoStorage, Rgb888, RgbColor};
+use embedded_graphics::pixelcolor::*;
 use embedded_graphics::prelude::DrawTarget;
 use embedded_graphics::primitives::Rectangle;
 use embedded_graphics::Pixel;
 
 struct ColorAdapter<'a, C, D>
 where
-    C: RgbColor,
+    C: RgbColor + FromRGB,
     D: DrawTarget<Color = C> + OriginDimensions,
 {
     state: &'a State,
@@ -18,7 +16,7 @@ where
 
 impl<'a, C, D> OriginDimensions for ColorAdapter<'a, C, D>
 where
-    C: RgbColor,
+    C: RgbColor + FromRGB,
     D: DrawTarget<Color = C> + OriginDimensions,
 {
     fn size(&self) -> embedded_graphics::prelude::Size {
@@ -28,7 +26,7 @@ where
 
 impl<'a, C, D> DrawTarget for ColorAdapter<'a, C, D>
 where
-    C: RgbColor,
+    C: RgbColor + FromRGB,
     D: DrawTarget<Color = C> + OriginDimensions,
 {
     type Color = Gray2;
@@ -38,7 +36,7 @@ where
     where
         I: IntoIterator<Item = Pixel<Self::Color>>,
     {
-        panic!("not implemented")
+        panic!("not implemented, use fill_contiguous instead")
     }
 
     fn fill_contiguous<I>(&mut self, area: &Rectangle, colors: I) -> Result<(), Self::Error>
@@ -47,12 +45,68 @@ where
     {
         let iter = colors.into_iter().map(|c: Gray2| -> C {
             let index = c.into_storage();
+            debug_assert!(index < 4);
             let rgb888 = self.state.palette[index as usize];
             let r = rgb888.r() as u32 * C::MAX_R as u32 / Rgb888::MAX_R as u32;
             let g = rgb888.g() as u32 * C::MAX_G as u32 / Rgb888::MAX_G as u32;
             let b = rgb888.b() as u32 * C::MAX_B as u32 / Rgb888::MAX_B as u32;
-            C::new(r, g, b) // oh no
+            debug_assert!(r < 256);
+            debug_assert!(g < 256);
+            debug_assert!(b < 256);
+            C::from_rgb(r as u8, g as u8, b as u8)
         });
         self.target.fill_contiguous(area, iter)
+    }
+}
+
+pub trait FromRGB {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self;
+}
+
+impl FromRGB for Rgb555 {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self::new(r, g, b)
+    }
+}
+
+impl FromRGB for Bgr555 {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self::new(r, g, b)
+    }
+}
+
+impl FromRGB for Rgb565 {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self::new(r, g, b)
+    }
+}
+
+impl FromRGB for Bgr565 {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self::new(r, g, b)
+    }
+}
+
+impl FromRGB for Rgb666 {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self::new(r, g, b)
+    }
+}
+
+impl FromRGB for Bgr666 {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self::new(r, g, b)
+    }
+}
+
+impl FromRGB for Rgb888 {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self::new(r, g, b)
+    }
+}
+
+impl FromRGB for Bgr888 {
+    fn from_rgb(r: u8, g: u8, b: u8) -> Self {
+        Self::new(r, g, b)
     }
 }
