4513740f265259d8ca910bec68c6d630c2d5dc45
gram <git@orsinium.dev>
2024-03-30 09:54:36 +0100

    stricter full ID validation

--- src/validators.rs
+++ src/validators.rs
@@ -1,32 +1,48 @@
+use core::str::Bytes;
+
 /// Validate the full app ID.
 ///
 /// The ID must contain the author ID and the short app ID separated by dot.
 /// Both parts should have at least one character and may contain only
 /// alphanumeric ASCII characters and hyphen.
 pub(crate) fn valid_full_id(s: &str) -> bool {
-    let mut dot_found = false;
-    let mut len = 0;
-    for c in s.bytes() {
-        len += 1;
+    let mut b = s.bytes();
+    // validate author ID
+    if !valid_id_part(&mut b) {
+        return false;
+    }
+    // validate app ID
+    if !valid_id_part(&mut b) {
+        return false;
+    }
+    if s.ends_with('.') {
+        return false;
+    }
+    // all bytes should be consumed (false if there is more than one dot)
+    b.next().is_none()
+}
+
+fn valid_id_part(b: &mut Bytes<'_>) -> bool {
+    let mut alpha_found = false;
+    for c in b {
+        // stop consuming when dot is encountered
         if c == b'.' {
-            // don't start with dot
-            if len == 1 {
+            break;
+        }
+        if c == b'-' {
+            // forbid starting with hyphen
+            if !alpha_found {
                 return false;
             }
-            // only one dot is allowed
-            if dot_found {
-                return false;
-            }
-            dot_found = true;
             continue;
         }
-        // TODO: forbid hyphen-only names
-        if !c.is_ascii_alphanumeric() && c != b'-' {
+        if !c.is_ascii_lowercase() && !c.is_ascii_digit() {
             return false;
         }
+        alpha_found = true;
     }
-    // TODO: require dot
-    (3..=40).contains(&len)
+    // forbid hyphen-only names
+    alpha_found
 }
 
 #[cfg(test)]
@@ -36,20 +52,26 @@ mod tests {
     #[test]
     fn test_valid_full_id() {
         assert!(valid_full_id("user.app"));
-        assert!(valid_full_id("User.App"));
         assert!(valid_full_id("some-user.some-app"));
-        assert!(valid_full_id("Some-User.Some-App"));
-        assert!(valid_full_id("user-name.APP"));
+        assert!(valid_full_id("user-name.app"));
+        assert!(valid_full_id("user.app-name"));
         assert!(valid_full_id("a.b"));
-        assert!(valid_full_id("A.B"));
 
         assert!(!valid_full_id("user.name.app")); // too many dots
         assert!(!valid_full_id("user_name.app")); // underscore is not allowed
         assert!(!valid_full_id("user name.app")); // whitespace is not allowed
         assert!(!valid_full_id("user.app_name")); // underscore is not allowed
+        assert!(!valid_full_id("User.app")); // uppercase letters in author
+        assert!(!valid_full_id("user.App")); // uppercase letters in app
         assert!(!valid_full_id("a")); // too short
         assert!(!valid_full_id("a.")); // too short
         assert!(!valid_full_id(".a")); // too short
-        assert!(!valid_full_id(".gamename")); // starts with dot
+        assert!(!valid_full_id("authorgame")); // no dot
+        assert!(!valid_full_id("author-game")); // no dot
+        assert!(!valid_full_id(".gamename")); // no author ID
+        assert!(!valid_full_id("authorname.")); // no app ID
+        assert!(!valid_full_id("author.game.")); // ends with dot
+        assert!(!valid_full_id(".author.game")); // starts with dot
+        assert!(!valid_full_id("author.name.game")); // too many dots
     }
 }
