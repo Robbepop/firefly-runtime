cd88f63ffb05c12baa811c4b0603f74b70b88dc8
gram <git@orsinium.dev>
2024-04-09 14:45:35 +0200

    replace 2 pads with 1, add 4 buttons instead

--- src/input.rs
+++ src/input.rs
@@ -3,13 +3,11 @@ use firefly_device::*;
 
 type C<'a> = wasmi::Caller<'a, State>;
 
-pub(crate) fn read_left(mut caller: C) -> u32 {
+pub(crate) fn read_pad(mut caller: C) -> u32 {
     let state = caller.data_mut();
     // TODO: cache input in state
     let input = match state.device.read_input() {
-        Some(InputState {
-            left: Some(left), ..
-        }) => left,
+        Some(InputState { pad: Some(pad), .. }) => pad,
         _ => return 0,
     };
     let x = input.x as u16 as u32;
@@ -17,6 +15,15 @@ pub(crate) fn read_left(mut caller: C) -> u32 {
     x << 16 | y
 }
 
-pub(crate) fn read_buttons(mut _caller: C) -> u32 {
-    todo!()
+pub(crate) fn read_buttons(mut caller: C) -> u32 {
+    let state = caller.data_mut();
+    // TODO: cache input in state
+    let Some(input) = state.device.read_input() else {
+        return 0;
+    };
+    let mut res: u32 = 0;
+    for button in input.buttons.into_iter().rev() {
+        res = (res << 1) | u32::from(button);
+    }
+    res
 }
