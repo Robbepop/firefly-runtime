71f2aea30e49ddd55b5745d25ea1ba26bd034a8e
gram <git@orsinium.dev>
2024-04-12 15:05:24 +0200

    fs.dump_file

--- src/fs.rs
+++ src/fs.rs
@@ -1,5 +1,5 @@
 use crate::state::State;
-use embedded_io::Read;
+use embedded_io::{Read, Write};
 use firefly_device::Device;
 use firefly_meta::validate_path_part;
 
@@ -52,7 +52,7 @@ pub(crate) fn load_file(
     load_file_inner(caller, "data", path_ptr, path_len, buf_ptr, buf_len)
 }
 
-pub(crate) fn load_file_inner(
+pub fn load_file_inner(
     mut caller: C,
     dir: &str,
     path_ptr: u32,
@@ -91,6 +91,55 @@ pub(crate) fn load_file_inner(
     if file_size != buf_len {
         let msg = "buffer size for file does not match the file size";
         state.device.log_error("fs", msg);
+        return 0;
+    }
+    file_size as u32
+}
+
+/// Create file in data dir and write into it the contents of the buffer.
+pub(crate) fn dump_file(
+    mut caller: C,
+    path_ptr: u32,
+    path_len: u32,
+    buf_ptr: u32,
+    buf_len: u32,
+) -> u32 {
+    let state = caller.data_mut();
+    let Some(memory) = state.memory else {
+        state.device.log_error("fs", "memory not found");
+        return 0;
+    };
+    let (data, state) = memory.data_and_store_mut(&mut caller);
+    let Some(name) = get_file_name(state, data, path_ptr, path_len) else {
+        return 0;
+    };
+
+    let path = &["data", &state.author_id, &state.app_id, name];
+    let Some(mut file) = state.device.create_file(path) else {
+        let msg = "cannot create file";
+        state.device.log_error("fs.dump_file", msg);
+        return 0;
+    };
+    let buf_ptr = buf_ptr as usize;
+    let buf_len = buf_len as usize;
+    let Some(buf) = data.get_mut(buf_ptr..(buf_ptr + buf_len)) else {
+        let msg = "buffer for file points out of memory";
+        state.device.log_error("fs.dump_file", msg);
+        return 0;
+    };
+    let Ok(file_size) = file.write(buf) else {
+        let msg = "cannot read file";
+        state.device.log_error("fs.dump_file", msg);
+        return 0;
+    };
+    if file.flush().is_err() {
+        let msg = "cannot flush file";
+        state.device.log_error("fs.dump_file", msg);
+        return 0;
+    }
+    if file_size != buf_len {
+        let msg = "not the whole buffer could be written into file";
+        state.device.log_error("fs.dump_file", msg);
         return 0;
     }
     file_size as u32
