adf0e5e83c22975aac9c15daa033c1748232d22e
gram <git@orsinium.dev>
2024-06-30 10:20:54 +0200

    test and improve RingBuf

--- src/ring.rs
+++ src/ring.rs
@@ -6,13 +6,14 @@ const MAX_DRIFT: usize = BUF_SIZE / 2;
 /// Circular buffer designed to keep a short history and a short look-ahead
 /// for netowrk device state updates. The goal is to be able to reply recent frames
 /// as well as not to loose frames received earlier than expected.
-pub(crate) struct RingBuf<T> {
+#[derive(Debug)]
+pub(crate) struct RingBuf<T: Copy> {
     frame: usize,
-    data:  [MaybeUninit<T>; BUF_SIZE],
+    data:  [Option<(usize, T)>; BUF_SIZE],
 }
 
-impl<T> RingBuf<T> {
-    const INIT: MaybeUninit<T> = MaybeUninit::uninit();
+impl<T: Copy> RingBuf<T> {
+    const INIT: Option<(usize, T)> = None;
 
     pub fn new() -> Self {
         Self {
@@ -32,7 +33,7 @@ impl<T> RingBuf<T> {
             return;
         }
         let index = frame % BUF_SIZE;
-        self.data[index] = MaybeUninit::new(val);
+        self.data[index] = Some((frame, val));
     }
 
     pub fn get(&mut self, frame: usize) -> Option<T> {
@@ -40,7 +41,58 @@ impl<T> RingBuf<T> {
             return None;
         }
         let index = frame % BUF_SIZE;
-        let val = self.data.get(index);
-        val.map(|v| unsafe { v.assume_init_read() })
+        let val = self.data.get(index)?;
+        let (act_frame, val) = (*val)?;
+        if act_frame != frame {
+            return None;
+        }
+        Some(val)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_ring_buf() {
+        let mut b: RingBuf<i32> = RingBuf::new();
+        for i in 0..20 {
+            assert_eq!(b.get(i), None);
+        }
+        for i in 0..20 {
+            b.insert(i, 60 + i as i32);
+        }
+        // only the current frame (0) and 2 frames ahead must be inserted
+        assert_eq!(b.get(0), Some(60));
+        assert_eq!(b.get(1), Some(61));
+        assert_eq!(b.get(2), Some(62));
+        assert_eq!(b.get(3), None);
+
+        // advance 10 frames forward
+        for _ in 0..10 {
+            b.advance();
+        }
+        assert_eq!(b.frame, 10);
+        // all existing old frames must be ignored
+        for i in 0..20 {
+            assert_eq!(b.get(i), None);
+        }
+        // insert lots of frames, only the current frame, 2 before, and 2 after
+        // must be inserted.
+        for i in 0..20 {
+            b.insert(i, 60 + i as i32);
+        }
+        for i in 0..=7 {
+            assert_eq!(b.get(i), None);
+        }
+        assert_eq!(b.get(8), Some(68));
+        assert_eq!(b.get(9), Some(69));
+        assert_eq!(b.get(10), Some(70));
+        assert_eq!(b.get(11), Some(71));
+        assert_eq!(b.get(12), Some(72));
+        for i in 13..=20 {
+            assert_eq!(b.get(i), None);
+        }
     }
 }
