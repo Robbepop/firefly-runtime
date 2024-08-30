53c1d6b3a6f1dfb55bff09bca98dcc9725e2c465
gram <git@orsinium.dev>
2024-08-30 20:41:03 +0200

    drop support for sinks, always output one channel

--- src/host/audio.rs
+++ src/host/audio.rs
@@ -6,21 +6,6 @@ use firefly_audio::*;
 
 type C<'a> = wasmi::Caller<'a, State>;
 
-pub(crate) fn get_sink(mut caller: C, index: u32) -> u32 {
-    let state = caller.data_mut();
-    state.called = "audio.get_sink";
-    let sink = match index {
-        1 => Sink::Adaptive,
-        2 => Sink::Headphones,
-        3 => Sink::Speakers,
-        _ => {
-            state.log_error(HostError::UnknownNode(index));
-            return 0;
-        }
-    };
-    sink.id()
-}
-
 pub(crate) fn add_sine(mut caller: C, parent_id: u32, freq: f32, phase: f32) -> u32 {
     let state = caller.data_mut();
     state.called = "audio.add_sine";
